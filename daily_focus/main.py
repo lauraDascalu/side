436ddedb3d23933e99b39976741eb5cbbd979f88
lauraDascalu <dascalulaura27@gmail.com>
2026-04-26 13:09:44 +0300

    daily_focus

---
+++ daily_focus/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from database import init_db, get_session
+from routes.routes import router
+from sqlmodel import Session, select
+from models import TaskLog
+from datetime import datetime
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+init_db()
+app.include_router(router)
+
